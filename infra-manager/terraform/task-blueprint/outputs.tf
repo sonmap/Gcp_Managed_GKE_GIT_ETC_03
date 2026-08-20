b05265e17fa0ab6014ec46aa840e1579fb130327
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-08-20 22:57:21 +0900

    Output task namespace details

--- infra-manager/terraform/task-blueprint/outputs.tf
+++ infra-manager/terraform/task-blueprint/outputs.tf
@@ -7,3 +7,13 @@ output "dataset_self_link" {
   value       = google_bigquery_dataset.task.self_link
   description = "Created BigQuery dataset self link"
 }
+
+output "namespace" {
+  value       = kubernetes_namespace_v1.task.metadata[0].name
+  description = "Created GKE namespace"
+}
+
+output "resource_quota" {
+  value       = kubernetes_resource_quota_v1.task.metadata[0].name
+  description = "Created Kubernetes ResourceQuota"
+}

26199bb1fcaece85a5bf587d50c88939e50ce2bf
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-08-20 22:57:02 +0900

    Add Kubernetes provider for task namespaces

--- infra-manager/terraform/task-blueprint/versions.tf
+++ infra-manager/terraform/task-blueprint/versions.tf
@@ -6,9 +6,27 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 6.0, < 8.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.30, < 3.0"
+    }
   }
 }
 
 provider "google" {
   project = var.target_project_id
+}
+
+data "google_client_config" "current" {}
+
+data "google_container_cluster" "analysis" {
+  project  = local.gke_project_id
+  name     = var.gke_cluster_name
+  location = var.gke_location
+}
+
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.analysis.endpoint}"
+  token                  = data.google_client_config.current.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.analysis.master_auth[0].cluster_ca_certificate)
 }
